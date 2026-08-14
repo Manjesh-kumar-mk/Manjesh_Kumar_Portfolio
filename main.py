2953dd7d06789a32b6f91b47dc99e6910f2af211
MANJESH KUMAR <145048515+Manjesh-kumar-mk@users.noreply.github.com>
2026-08-14 23:03:13 +0530

    add origin of website

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "https://manjesh-kumar-portfolio.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
