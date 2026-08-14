0c398cba72090d60d94c192fbad69f3b675b08fd
Manjesh_Kumar_mk <manjeshkumar962144@gmail.com>
2026-08-14 22:32:54 +0530

    fix app.get

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request, "index.html", {"request": request})
 
 app.add_middleware(
     CORSMiddleware,
